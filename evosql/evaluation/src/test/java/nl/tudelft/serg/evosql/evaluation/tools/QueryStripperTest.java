ab6d6349dd1bcf93fc7c44e2944b3d0ef5c13101
Nick Heppert <nick.heppert@gmx.de>
2018-10-29 14:33:50 +0900

    Correct commit: https://github.com/SERG-Delft/evosql/commit/e2dadbd9096080b72e74f398444f9a890c3ccabe#diff-8c333bd91d536a39054c73c5785a56b4 Still tests fail

--- evosql/evaluation/src/test/java/nl/tudelft/serg/evosql/evaluation/tools/QueryStripperTest.java
+++ evosql/evaluation/src/test/java/nl/tudelft/serg/evosql/evaluation/tools/QueryStripperTest.java
@@ -8,60 +8,60 @@ public class QueryStripperTest {
 	@Test
 	public void stripSelectAllQuery() {
 		String sql = new QueryStripper("SELECT * FROM Table").getStrippedSql();
-		Assertions.assertEquals(sql, "SELECT * FROM \"TABLE\"");
+		Assertions.assertEquals(sql, "SELECT * FROM \"Table\"");
 	}
 	
 	@Test
 	public void stripInteger() {
 		String sql = new QueryStripper("SELECT * FROM Table WHERE col = 10").getStrippedSql();
-		Assertions.assertEquals(sql, "SELECT * FROM \"TABLE\" WHERE \"COL\" = 0");
+		Assertions.assertEquals(sql, "SELECT * FROM \"Table\" WHERE \"col\" = 0");
 	}
 
 	@Test
 	public void stripString() {
 		String sql = new QueryStripper("SELECT * FROM Table WHERE col = 'test'").getStrippedSql();
-		Assertions.assertEquals(sql, "SELECT * FROM \"TABLE\" WHERE \"COL\" = ''");
+		Assertions.assertEquals(sql, "SELECT * FROM \"Table\" WHERE \"col\" = ''");
 	}
 
 	@Test
 	public void stripDouble() {
 		String sql = new QueryStripper("SELECT * FROM Table WHERE col = 50.1234").getStrippedSql();
-		Assertions.assertEquals(sql, "SELECT * FROM \"TABLE\" WHERE \"COL\" = 0");
+		Assertions.assertEquals(sql, "SELECT * FROM \"Table\" WHERE \"col\" = 0");
 	}
 
 	@Test
 	public void stripDate() {
 		String sql = new QueryStripper("SELECT * FROM Table WHERE col = '1999-03-04'").getStrippedSql();
-		Assertions.assertEquals(sql, "SELECT * FROM \"TABLE\" WHERE \"COL\" = '2000-01-01'");
+		Assertions.assertEquals(sql, "SELECT * FROM \"Table\" WHERE \"col\" = '2000-01-01'");
 	}
 
 	@Test
 	public void stripDate2() {
 		String sql = new QueryStripper("SELECT * FROM Table WHERE col = '1999-3-4'").getStrippedSql();
-		Assertions.assertEquals(sql, "SELECT * FROM \"TABLE\" WHERE \"COL\" = '2000-01-01'");
+		Assertions.assertEquals(sql, "SELECT * FROM \"Table\" WHERE \"col\" = '2000-01-01'");
 	}
 
 	@Test
 	public void stripTime() {
 		String sql = new QueryStripper("SELECT * FROM Table WHERE col = '1:13:56'").getStrippedSql();
-		Assertions.assertEquals(sql, "SELECT * FROM \"TABLE\" WHERE \"COL\" = '12:00:00'");
+		Assertions.assertEquals(sql, "SELECT * FROM \"Table\" WHERE \"col\" = '12:00:00'");
 	}
 
 	@Test
 	public void stripTime2() {
 		String sql = new QueryStripper("SELECT * FROM Table WHERE col = '14:13:56'").getStrippedSql();
-		Assertions.assertEquals(sql, "SELECT * FROM \"TABLE\" WHERE \"COL\" = '12:00:00'");
+		Assertions.assertEquals(sql, "SELECT * FROM \"Table\" WHERE \"col\" = '12:00:00'");
 	}
 
 	@Test
 	public void stripTimestamp() {
 		String sql = new QueryStripper("SELECT * FROM Table WHERE col = '1999-03-04 01:13:56'").getStrippedSql();
-		Assertions.assertEquals(sql, "SELECT * FROM \"TABLE\" WHERE \"COL\" = '2000-01-01 12:00:00'");
+		Assertions.assertEquals(sql, "SELECT * FROM \"Table\" WHERE \"col\" = '2000-01-01 12:00:00'");
 	}
 
 	@Test
 	public void stripTimestamp2() {
 		String sql = new QueryStripper("SELECT * FROM Table WHERE col = '1999-3-4 13:13:56.100'").getStrippedSql();
-		Assertions.assertEquals(sql, "SELECT * FROM \"TABLE\" WHERE \"COL\" = '2000-01-01 12:00:00'");
+		Assertions.assertEquals(sql, "SELECT * FROM \"Table\" WHERE \"col\" = '2000-01-01 12:00:00'");
 	}
 }
