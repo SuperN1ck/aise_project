5f02adf5fd31dd43aa8074998effa545b0afa497
Nick Heppert <nick.heppert@gmx.de>
2018-11-30 00:43:08 +0900

    Remove long comment

--- evosql/ga/src/main/java/nl/tudelft/serg/evosql/EvoSQLMOO.java
+++ evosql/ga/src/main/java/nl/tudelft/serg/evosql/EvoSQLMOO.java
@@ -62,8 +62,7 @@ public class EvoSQLMOO extends EvoSQLSolver{
 		allPaths.stream().forEach(path -> log.debug(path));
 		
 		Map<String, TableSchema> tableSchemas;
-		Seeds seeds; // TODO Is this needed, maybe something like SeedsMOO?
-		
+        
         Seeds seeds; 
         if (EvoSQLConfiguration.USE_LITERAL_SEEDING) {
             // Get the seeds for the current path
@@ -85,39 +84,7 @@ public class EvoSQLMOO extends EvoSQLSolver{
         tableSchemas = schemaExtractor.getTablesFromQuery(sqlToBeTested);
 
         Result result = new Result(sqlToBeTested, System.currentTimeMillis());
-
-        // TODO implement MOO
-        /* [Nick] Some thougts I had:
-         * - A "Fixture" is a individual
-         * - In StandardGA.calculateFitness(Fixture fixture)
-         * ---- fitness is calculated for one explicit solution
-         * ---- As this methods is in the StandardGA-class it uses some class
-         *      variables. Especially, in this line
-         *          genetic.Instrumenter.execute(pathToTest);
-         *      where the query plan is run and analyzed
-         * - We therefore should refactor this function 
-         * ---- Decouple it from StandardGA-class
-         * ---- Make sure individualCount++ is still increased
-         * ---- We should leave calculateFitness in StandardGA
-         * ---- I already created a file named FixtureFitnessCalculator
-         * - With this refactoring comes a decision how deep we should modify 
-         *   EvoSQL. Mainly this question raises because this would require a
-         *   deeper modification in FixtureFitness. Right now it is assumed that
-         *   That there is only one objective.
-         * - The simplest possibility would be to completly develop orthogonal 
-         *   to the current implementation and mostly copy and paste things just 
-         *   as in this file.
-         * - In FixtureComparotr.compare(Fixture o1, Fixture o2) two fixtures
-         *   are compared. If they have the same level, the result of the 
-         *   comparison for this level is returned
-         * - In QueryLevelData.compare(QueryLeveldData qld1, QueryLevelData qld2)
-         *   two results of a level are getting compared against each other
-         * - We can look at these implementation to see and understand how the 
-         *   query level processing works 
-         * - --> From that we can derive how and what to change to formulate as
-         *   MOO.
-        */
-        
+       
         try {
             // Create schema on instrumenter
             for (TableSchema ts : tableSchemas.values()) {
