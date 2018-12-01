4f8b211c2a76bdac7ff2c85a9b1190f47e0f023d
Rienshaliu <rienshaliu@gmail.com>
2018-12-01 16:04:48 +0900

    import NSGA-II hashset

--- evosql/ga/src/main/java/nl/tudelft/serg/evosql/metaheuristics/NSGAII.java
+++ evosql/ga/src/main/java/nl/tudelft/serg/evosql/metaheuristics/NSGAII.java
@@ -7,6 +7,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.HashSet;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
