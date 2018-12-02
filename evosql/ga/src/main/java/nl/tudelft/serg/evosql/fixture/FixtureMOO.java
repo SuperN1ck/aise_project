b08d441f9f4116845fe195dbc06abf8efcbe6c80
Nick Heppert <nick.heppert@gmx.de>
2018-12-02 17:33:55 +0900

    Report database size in fitness calculation --> the bigger the slower

--- evosql/ga/src/main/java/nl/tudelft/serg/evosql/fixture/FixtureMOO.java
+++ evosql/ga/src/main/java/nl/tudelft/serg/evosql/fixture/FixtureMOO.java
@@ -1,14 +1,21 @@
 package nl.tudelft.serg.evosql.fixture;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import nl.tudelft.serg.evosql.metaheuristics.operators.FixtureFitness;
 import nl.tudelft.serg.evosql.sql.TableSchema;
 
 public class FixtureMOO extends Fixture {
+    private static Logger log = LogManager.getLogger(FixtureMOO.class);
+
     private List<FixtureFitness> fitness_moo = new ArrayList<FixtureFitness>();
     private double crowdingDistance;
 
@@ -72,6 +79,21 @@ public class FixtureMOO extends Fixture {
             genetic.Instrumenter.execute(sqlStatement);
         }
 
+        Statement st = genetic.Instrumenter.getStatement();
+        for (String table_name : tableSchemas.keySet()) {
+            try {
+                st.execute("SELECT COUNT(*) as \"row_count\" FROM \"" + table_name + "\"");
+                ResultSet rs = st.getResultSet();
+                if (rs.next())
+                    log.debug("Current row count in " + table_name + ": " + rs.getInt("row_count"));
+                else
+                    throw new SQLException("Table " + table_name + " not in data base");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        int paths_covered = 0;
         for (String path_to_test : paths_to_test) {
             // Start instrumenter
             genetic.Instrumenter.startInstrumenting();
@@ -96,8 +118,7 @@ public class FixtureMOO extends Fixture {
         return evaluations;
     }
 
-    public int getCoveredTargets()
-    {
+    public int getCoveredTargets() {
         int coveredTargets = 0;
         for (FixtureFitness ff : fitness_moo)
             coveredTargets += ff.getDistance() == 0. ? 1 : 0;
